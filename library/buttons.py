5415563bb5485c8ba9b65b87c6d3a2da968944e9
MMMoviesStore <88924298+MMMoviesStore@users.noreply.github.com>
2021-12-18 23:14:39 +0630

    Update buttons.py

--- library/buttons.py
+++ library/buttons.py
@@ -21,8 +21,7 @@ home_button = [
 
 
 start_button = [
-    [InlineKeyboardButton("🏅 GitHub 🏅", url="https://t.me/TheLawliet"),
-     InlineKeyboardButton("⚙️Settings ⚙", "start_btn")]
+    InlineKeyboardButton("⚙️Settings ⚙", "start_btn")]
 ]
 
 
@@ -49,7 +48,6 @@ finished_button = [
 
 
 terminate_btn = [
-    [InlineKeyboardButton("🧸 Updates", url="https://github.com/m4mallu/clonebot-ui"),
      InlineKeyboardButton("❓ Usage", url="https://telegra.ph/Clonebot-UI-Help-05-30")],
     [InlineKeyboardButton("🚫 Terminate", "terminate_btn"),
      InlineKeyboardButton("🏠 Home", "start_btn")]
